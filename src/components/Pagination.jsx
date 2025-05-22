1d200637ae1e6d7c24c2cf096d847c12e29fcd35
shamshersarpal <shamshersingh01567@gmail.com>
2025-05-22 16:32:45 +0530

    Browse Domains

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -16,7 +16,7 @@ export default function Pagination() {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width={24}
-                  height={24}
+                  height={24}             
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
